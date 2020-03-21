064fe584b9130b065e22c1a23dffb08aa150b76f
Den Tre <54660529+DEN-TRE@users.noreply.github.com>
2020-03-21 23:09:50 +0300

    css correction

--- client/components/ToDoList/Button-Done.jsx
+++ client/components/ToDoList/Button-Done.jsx
@@ -5,7 +5,7 @@ const ButtonDone = () => {
     // eslint-disable-next-line react/button-has-type
     <div className="h-12 w-12 flex items-center justify-center text-2xl font-semibold rounded-lg mr-2 my-2 overflow-hidden">
       <button type="button" onClick="" className="focus:outline-none mb-px ml-px">
-        <i className="far fa-check-circle text-green-500 box-shadow" />
+        <i className="far fa-check-circle text-green-500 box-shadow box-shadow-hover-done" />
       </button>
     </div>
   )
